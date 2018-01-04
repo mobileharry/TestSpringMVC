fb8d81d70fcb1dba573f3e5f2131309b17a64788
harry.chen@zhan.com <harry.chen@zhan.com>
2018-01-04 22:29:40 +0800

    a

--- src/com/demo/web/controllers/GoodsController.java
+++ src/com/demo/web/controllers/GoodsController.java
@@ -15,7 +15,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import org.apache.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.LoggerContext;
+
 
 import com.demo.web.entities.Goods;
 import com.demo.web.services.GoodsService;
@@ -24,8 +27,10 @@ import com.demo.web.services.GoodsService;
 @RequestMapping("/goods")
 public class GoodsController {
 	
-	private static final Logger logger = Logger.getLogger(GoodsController.class);
-	
+	//private static final Logger logger = Logger.getLogger(GoodsController.class);
+	private static final Logger logger = LogManager.getLogger(GoodsController.class);
+
+	 
 	@Resource
 	GoodsService goodsService;
 	
@@ -34,10 +39,8 @@ public class GoodsController {
 	 */
 	@RequestMapping("/list")
 	public String list(Model model,@RequestParam(required=false,defaultValue="1") int pageNO){
-		
 		logger.info("This is list goods info page");
 		logger.error("This is list goods error page");
-		
 		
 		int size=5;
 		model.addAttribute("size",size);
